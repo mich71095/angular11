6ce0c3e0e613157c1b9bab6dd704cabb55c1b413
Michael <michaelpsy71095@gmail.com>
2021-01-17 23:42:15 +0800

    Clear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -18,11 +13,6 @@ import { CartComponent } from './cart/cart.component';
   ],
   declarations: [
     AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent,
-    ProductDetailsComponent,
-    CartComponent
   ],
   bootstrap: [ AppComponent ]
 })
