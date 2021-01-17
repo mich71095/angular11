f1636442cf32020c24c19ab75cb0971f03c11b47
Michael <michaelpsy71095@gmail.com>
2021-01-18 01:11:23 +0800

    Edit heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,8 +9,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 @NgModule({
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
     AppRoutingModule,
+    FormsModule
   ],
   declarations: [
     AppComponent,
